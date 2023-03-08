36e2bc3622c2a1cbd3615d2d9536fc8b349a2935
cyyyyyyyyyyyyy <ily2407@gmail.com>
2023-03-08 06:48:39 +0300

    added polymorphic variants and file output

--- main.ml
+++ main.ml
@@ -1,17 +1,17 @@
-open Printf;;
+open Printf ;;
+(*let _ = printf "%s" "ggg" ;; *)
 
-type qmlProp = 
-{ 
-  propId : string; 
-  propVal : string 
-};;
+type qmlProp =
+{
+  propId : string;
+  propVal : string  
+}
 
 type qmlObj = 
-{ 
-  title : string; 
-  qmlObjs : qmlObj list;
-  qmlProps : qmlProp list
-} ;;
+{
+  title : string;
+  qmlNodes : [`QmlProp of qmlProp | `QmlObj of qmlObj] list;
+}
 let rec tab n = 
   match n with
   | 0 -> ""
@@ -28,20 +28,39 @@ let rec qmlObjListToString list tp = (*tp - tabulation parameter*)
   match list with
   | [] -> ""
   | hd :: tl -> 
-      tab tp ^ hd.title ^ "\n" 
-      ^ tab tp ^ "{" ^ "\n"
-      ^ propListToString hd.qmlProps (tp + 1)
-      ^ qmlObjListToString hd.qmlObjs (tp + 1)     
+     let base = tab tp ^ hd.title ^ "\n" 
+      ^ tab tp ^ "{" ^ "\n" in
+
+      let rec f (nodes : [`QmlProp of qmlProp | `QmlObj of qmlObj] list) tp =
+        match nodes with
+        | [] -> ""
+        | nodehd :: nodetl ->
+          match nodehd with
+          | `QmlProp qp -> propListToString [qp] tp
+          | `QmlObj qo -> qmlObjListToString [qo] tp;
+        ^ f nodetl tp 
+        in
+
+      base ^
+      f hd.qmlNodes (tp + 1) 
       ^ tab tp ^ "}" ^ "\n"
-      ^ qmlObjListToString tl (tp) ;;
+      ^ qmlObjListToString tl (tp) 
+;;
 
-let textprops = [{propId = "id"; propVal = "text1"}; {propId = "x"; propVal = "40"}; {propId = "y"; propVal = "10"}; {propId = "text"; propVal = "hi"} ] ;;
-let text = {title = "Text"; qmlObjs = []; qmlProps = textprops} ;;
-let rectangle1props = [{propId = "id"; propVal = "rect1"}] ;;
-let rectangle1 = {title = "Rectangle"; qmlObjs = [text]; qmlProps = rectangle1props} ;;
-let imageprops = [{propId = "id"; propVal = "image1"}; {propId = "x"; propVal = "60"}; {propId = "y"; propVal = "30"}; {propId = "image"; propVal = "url"}] ;;
-let image = {title = "Image"; qmlObjs = []; qmlProps = imageprops } ;;
-let rectangle2props = [{propId = "id"; propVal = "rect2"}] ;;
-let rectangle2 = {title = "Rectangle"; qmlObjs = [text; rectangle1]; qmlProps = rectangle2props} ;;
+let textprops = [`QmlProp {propId = "id"; propVal = "text1"}; `QmlProp {propId = "x"; propVal = "40"}; 
+                 `QmlProp {propId = "y"; propVal = "10"}; `QmlProp {propId = "text"; propVal = "hi"} ] ;;
+let text = {title = "Text"; qmlNodes = textprops; } ;;
+let rectangle1props = [`QmlProp {propId = "id"; propVal = "rect1"}] ;;
+let rectangle1 = {title = "Rectangle"; qmlNodes = [`QmlProp {propId = "x"; propVal = "31"}] @ [`QmlObj text] @ rectangle1props} ;;
+let imageprops = [`QmlProp {propId = "id"; propVal = "image1"}; `QmlProp {propId = "x"; propVal = "60"}; 
+                  `QmlProp {propId = "y"; propVal = "30"}; `QmlProp {propId = "image"; propVal = "url"}] ;;
+let image = {title = "Image"; qmlNodes = imageprops } ;;
+let rectangle2props = [`QmlProp {propId = "id"; propVal = "rect2"}] ;;
+let rectangle2 = {title = "Rectangle"; qmlNodes = rectangle2props @ [`QmlObj image] @ [`QmlProp {propId = "x"; propVal = "31"}] @ [`QmlObj rectangle1]} ;;
 
-let _ = printf "%s" (qmlObjListToString [rectangle2] 0) ;;
+let s = qmlObjListToString [rectangle2] 0 ;;
+let _ = printf "%s" s ;;
+let __ = 
+  let oc = open_out "qmltest.qml" in
+  fprintf oc "%s" s; 
+  close_out oc ;;
