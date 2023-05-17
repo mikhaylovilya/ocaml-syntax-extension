457c6fe4a3c680fc663e4268e695b17bd5904471
cyyyyyyyyyyyyy <ily2407@gmail.com>
2023-05-17 20:19:16 +0100

    Update caml_dynamic_qobj.ml

--- caml_dynamic/caml_dynamic_qobj.ml
+++ caml_dynamic/caml_dynamic_qobj.ml
@@ -1,27 +1,29 @@
 module CamlDynamicQObj : sig
-  (*bool emitDynamicSignal(char *signal, void **arguments);
-    bool connectDynamicSlot(QObject *obj, char *signal, char *slot);
-    bool connectDynamicSignal(char *signal, QObject *obj, char *slot);*)
+  type 'a mycppobj = [ `Cppobj ]
   type t
 
   val create : unit -> t
-  val create_slot : t -> name:string -> slot:(unit -> unit) -> unit
-  val connect_slot : unit -> unit (*dummy*)
-  val connect_signal : unit -> unit (*dummy*)
-  val emit_signal : unit -> unit (*dummy*)
+  val create_func : t -> name:string -> f:(unit -> unit) -> unit
+  val to_lablqml_cppobj : t -> t mycppobj
+
+  (* val create_slot : t -> name:string -> slot:(unit -> unit) -> unit
+     val connect_slot : unit -> unit
+     val connect_signal : unit -> unit
+     val emit_signal : unit -> unit *)
+  (*bool emitDynamicSignal(char *signal, void **arguments);
+    bool connectDynamicSlot(QObject *obj, char *signal, char *slot);
+    bool connectDynamicSignal(char *signal, QObject *obj, char *slot);*)
 end = struct
-  type 'a cppobj = [ `Cppobj ]
-  type t = t cppobj
+  type 'a mycppobj = [ `Cppobj ]
+  type t = t mycppobj
 
   external create_stub : unit -> t = "caml_create_camldynamicqobj"
 
   let create () = create_stub ()
 
-  external create_slot_stub : t -> string -> (unit -> unit) -> unit
-    = "caml_create__dynamic_slot"
+  external create_func_stub : t -> string -> (unit -> unit) -> unit
+    = "caml_create_func"
 
-  let create_slot t ~name ~slot = create_slot_stub t name slot
-  let connect_slot () = ()
-  let connect_signal () = ()
-  let emit_signal () = ()
+  let create_func t ~name ~f = create_func_stub t name f
+  let to_lablqml_cppobj : t -> t mycppobj = fun x -> x
 end
